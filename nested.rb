0620c71b1639f5cd6c949723c3e8fbcf490f1951
Kevin Iturralde <keviniturralde3@gmail.com>
2020-07-31 03:35:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -86,6 +86,7 @@ def adding_matz
  		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
  		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
  		 programmer_hashend
+	 end
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable.
